bd4754f63ba80f8981c96750a359479d0625e60f
jeetpatel313 <jp2398d@gmail.com>
2023-03-04 13:14:27 -0800

    updated notations

--- contracts/IdjotToken.sol
+++ contracts/IdjotToken.sol
@@ -7,6 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract IdjotErc20 is ERC20, Ownable, ERC20Burnable {
     constructor() ERC20("Token", "TOK") {
-        _mint(owner(), 100000000 * 1e18);
+        _mint(owner(), 100000000 * 10**decimals());
     }
 }
