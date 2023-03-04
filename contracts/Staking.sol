8c56c050efc8c668131f55938c412e6a706bb282
jeetpatel313 <jp2398d@gmail.com>
2023-03-04 11:53:36 -0800

    updated contracts to support multiple nftId staking

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -11,31 +11,39 @@ contract stakingContract is IdjotErc20, IdjotErc1155 {
     constructor(address payable _depositAddress) {
         depositAddress = _depositAddress;
     }
-    
+
     struct stakedItem {
-        uint256 nftAmount;              // nft amount
-        uint256 nftId;                  // nft Id
-        uint256 stakeAtTime;            // timestamp at which nft was staked
-        address payable owner;          // address of the owner
-        bool isStaked;                  // staked or not
+        uint256 nftId; // nft Id
+        uint256 nftAmount; // nft amount
+        uint256 stakeAtTime; // timestamp at which nft was staked
+        address payable owner; // address of the owner
+        bool isStaked; // staked or not
     }
 
     mapping(address => stakedItem) private stakedInfo;
 
     event Staked(uint256 NftId, uint256 NftAmount, address stakedAddress);
-    
-    event UnStaked(uint256 NftId, uint256 NftAmount, address stakedAddress, uint256 rewardedAmt);
+
+    event UnStaked(
+        uint256 NftId,
+        uint256 NftAmount,
+        address stakedAddress,
+        uint256 rewardedAmt
+    );
 
     // update deposit address
-    function updateDepositAddress(address payable _newDepositAddress) public onlyOwner {
+    function updateDepositAddress(address payable _newDepositAddress)
+        public
+        onlyOwner
+    {
         depositAddress = _newDepositAddress;
     }
 
     // calculate the erc20 tokens rewarded
-    function calculate(uint256 stakedTime) internal view returns(uint256) {
+    function calculate(uint256 stakedTime) internal view returns (uint256) {
         uint256 calulationTime = block.timestamp - stakedTime;
         // calulation assumes that the 1 month = 30 days
-        if((calulationTime / 2628288) <= 1) {
+        if ((calulationTime / 2628288) <= 1) {
             return 500;
         } else if ((calulationTime / 2628288) <= 6) {
             return 1000;
@@ -46,37 +54,66 @@ contract stakingContract is IdjotErc20, IdjotErc1155 {
         }
     }
 
-    function stakeNFT(uint256 _nftId, uint256 _nftAmount) public returns(bool){
-        // staker should have the required _nftId and _nftAmount to stake
-        require(balanceOf(msg.sender, _nftId) >= _nftAmount, "You don't have required amount of the specified NFT ID");
-
-        // nftId must not be already staked
-        require(stakedInfo[msg.sender].isStaked == false, "NFT is already staked!");
-
-        // transfer the amount of nfts to the deposit address
-        safeTransferFrom(msg.sender, depositAddress, _nftId, _nftAmount, "");        
-
-        // update stakedItem 
-        stakedInfo[msg.sender] = stakedItem (
-            _nftAmount,
+    function updateStakedInfo(uint256 _nftId, uint256 _nftAmount) internal {
+        // update stakedItem
+        stakedInfo[msg.sender] = stakedItem(
             _nftId,
+            _nftAmount,
             block.timestamp,
             payable(msg.sender),
             true
         );
+    }
 
+    function stakeNFT(uint256 _nftId, uint256 _nftAmount)
+        public
+        returns (bool)
+    {
+        // staker should have the required _nftId and _nftAmount to stake
+        require(
+            balanceOf(msg.sender, _nftId) >= _nftAmount,
+            "You don't have required amount of the specified NFT ID"
+        );
+
+        // if user has already staked the _nftId before
+        if (stakedInfo[msg.sender].nftId == _nftId) {
+            // update stakedItem
+            updateStakedInfo(
+                _nftId,
+                _nftAmount + stakedInfo[msg.sender].nftAmount
+            );
+        }
+        // if user has not already staked the _nftId before
+        else {
+            // update stakedItem
+            updateStakedInfo(_nftId, _nftAmount);
+        }
+
+        // transfer the amount of nfts to the deposit address
+        safeTransferFrom(msg.sender, depositAddress, _nftId, _nftAmount, "");
+
+        // emit the Staked event
         emit Staked(_nftId, _nftAmount, msg.sender);
 
         return true;
     }
 
-    function unStakeNft(uint256 _nftId, uint256 _nftAmount) public returns(uint256) {
-        // require the user has staked the specified nft id 
-        require(stakedInfo[msg.sender].nftId == _nftId, "You don't have any staked item under this id");
+    function unStakeNft(uint256 _nftId, uint256 _nftAmount)
+        public
+        returns (uint256)
+    {
+        // require the user has staked the specified nft id
+        require(
+            stakedInfo[msg.sender].nftId == _nftId,
+            "You don't have any staked item under this id"
+        );
 
         // require the user has staked the specified amount or less
-        require(stakedInfo[msg.sender].nftAmount >= _nftAmount, "You have staked lesser value of this id");
-    
+        require(
+            stakedInfo[msg.sender].nftAmount >= _nftAmount,
+            "You have staked lesser value of this id"
+        );
+
         // calculate rewards
         uint256 rewards = calculate(stakedInfo[msg.sender].stakeAtTime);
 
@@ -87,9 +124,9 @@ contract stakingContract is IdjotErc20, IdjotErc1155 {
         _transfer(depositAddress, msg.sender, rewards);
 
         // update the stakeInfo
-        stakedInfo[msg.sender] = stakedItem (
-            0,
-            0,
+        stakedInfo[msg.sender] = stakedItem(
+            stakedInfo[msg.sender].nftId,
+            stakedInfo[msg.sender].nftAmount - _nftAmount,
             block.timestamp,
             payable(msg.sender),
             false
