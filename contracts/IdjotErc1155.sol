4067cbe28799056142a0e831e18550af0dc2a073
jeetpatel313 <jp2398d@gmail.com>
2023-03-03 18:08:05 -0800

    updated 1155 contract

--- contracts/IdjotErc1155.sol
+++ contracts/IdjotErc1155.sol
@@ -1,28 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.7.0 < 0.9.0;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 contract IdjotErc1155 is ERC1155, Ownable, ERC1155Burnable {
+    // to do : track how many tokens of what id have been minted already : give a max size of 10 nfts of every id
+
     constructor() ERC1155("") {}
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
-    {
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
         _mint(account, id, amount, data);
-    }
-
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mintBatch(to, ids, amounts, data);
     }
 }
